01905834c430f4c2a702579fb87eb16d4776cb7d
Fig Newton Reed <robertalexreed@gmail.com>
2020-07-15 21:18:34 -0600

    maybe m Please enter the commit message for your changes. Lines starting

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+    // Database Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '[N1n39898]');
+    define('DB_NAME', 'CSIS2440');
+
+    // App Root
+    define('APPROOT', dirname(dirname(dirname(__FILE__))));
+    // URL Root
+    define('URLROOT', 'http://ec2-18-236-162-239.us-west-2.compute.amazonaws.com');
+    // Site Name
+    define('SITENAME', 'CSIS2440');
+
--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
 <?php require_once '../app/bootstrap.php';
+// something different
 // Initialize the Core Library
 $initial = new Core;
